edf5802fb69908dee7feb61c9f58ede6cd087367
Ryan Milvenan <ryanmilvenan@gmail.com>
2015-03-02 16:44:09 -0700

    Added api key to public/javascripts/

---
+++ public/javascripts/keys.js
@@ -0,0 +1,5 @@
+var keys = {
+    mashape: '1t37z6ZtlvmshEIKo41r9f2yjIh1p14TpsnjsnnL0F5emFdSPa'
+};
+
+module.exports = keys;
--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,11 @@
 var express = require('express');
 var router = express.Router();
+var keys = require('../public/javascripts/keys')
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
+  console.log(keys.mashape)
 });
 
 module.exports = router;
